1bc0d0b900a8e7bb2aa8e8908e53e7ad72fea14f
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-06-06 09:03:49 -0300

    Modify new features whatsapp 😎

--- src/whatsapp.ts
+++ src/whatsapp.ts
@@ -1,8 +1,18 @@
 import { Whatsapp } from "venom-bot";
+import { sendMessage } from "./ia";
 
 async function main(whatsapp: Whatsapp) {
-  whatsapp.onAnyMessage(async (message) => {
-    await whatsapp.sendText(message.chatId, "Hello World");
+  whatsapp.onMessage(async (message) => {
+    try {
+      const response = await sendMessage(message);
+      const textClient = response.output;
+      // textClient.generic.forEach(async ({ text }) => {
+      //   await whatsapp.reply(message.from, text, message.id);
+      // });
+      console.log(textClient.generic);
+    } catch (error) {
+      console.log(error.message);
+    }
   });
 }
 
